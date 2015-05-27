372572be94bf4d2b021ae76ec40db2fdd31b8817
linxihui <xihuil.silence@gmail.com>
2015-05-26 23:30:49 -0400

    complete documentation

--- R/Gaussian_Process_Survival.R
+++ R/Gaussian_Process_Survival.R
@@ -2,10 +2,9 @@
 #' @description It works for classification, regression, cox regression and poission regression
 #' The first/main purpose is to implement Gaussian process into Cox's model
 #' The glmnet is used to solve a general Ridge-regularized regression solver. This may be changed in the future.
-#' @param x Design matrix (NO intercept)
-#' @param y Reponse vector of type double, integer, factor or Surv
-#' @param kernel, kpar check kernlab::gausspr
-#' @param family check glmnet::glmnet
+#' @param formula Model formula
+#' @param data Data
+#' @param ... Further argument passed to internal functions
 #' @return A 'gprsc' object
 #' @examples 
 #' ## classification
@@ -60,6 +59,15 @@ gpsrc.formula <- function(formula, data, ...) {
 	return(out);
 	}
 
+#' @param x Design matrix (NO intercept)
+#' @param y Reponse vector of type double, integer, factor or Surv
+#' @param scaled Logical value indicating if to standardize x, y
+#' @param kernel String or 'kernel' object (see kernlab::gausspr)
+#' @param kpar A list of Kernel parameters or 'automatic' if a radial kernel specified
+#' @param var Variance of response (from the Gaussian process). Only for regression
+#' @param family Options are 'gaussian', 'binomial', 'multinomial', 'poisson', 'cox'
+#' @rdname gpsrc
+#' @export gpsrc.default
 gpsrc.default <- function(
 	x, y, scaled = TRUE, 
 	kernel = 'rbfdot', kpar = 'automatic',
@@ -70,95 +78,95 @@ gpsrc.default <- function(
 		'integer' = 'poisson', 
 		'gaussian'), 
 	...) {
-		stopifnot(suppressMessages(require(glmnet)));
-		stopifnot(suppressMessages(require(kernlab)));
-		x.scale <- NULL;
-		y.scale <- NULL;
-		if (scaled) {
-			x <- scale(x);
-			x.scale <- attributes(x)[c('scaled:center', 'scaled:scale')];
-			attributes(x)[c('scaled:center', 'scaled:scale')] <- NULL;
-			if ('gaussian' == family) {
-				y <- scale(y);
-				y.scale <- attributes(y)[c('scaled:center', 'scaled:scale')];
-				attributes(y)[c('scaled:center', 'scaled:scale')] <- NULL;
-				if(1 == ncol(y)) y <- c(y);
-				}
+	stopifnot(suppressMessages(require(glmnet)));
+	stopifnot(suppressMessages(require(kernlab)));
+	x.scale <- NULL;
+	y.scale <- NULL;
+	if (scaled) {
+		x <- scale(x);
+		x.scale <- attributes(x)[c('scaled:center', 'scaled:scale')];
+		attributes(x)[c('scaled:center', 'scaled:scale')] <- NULL;
+		if ('gaussian' == family) {
+			y <- scale(y);
+			y.scale <- attributes(y)[c('scaled:center', 'scaled:scale')];
+			attributes(y)[c('scaled:center', 'scaled:scale')] <- NULL;
+			if(1 == ncol(y)) y <- c(y);
 			}
-		if(is.character(kernel)) {
-			kernel <- match.arg(
-				kernel, 
-				c(
-					"rbfdot","polydot","tanhdot","vanilladot",
-					"laplacedot","besseldot","anovadot","splinedot"
-					)
-				);
-			if(is.character(kpar)) {
-				if((kernel %in% c("tanhdot", "vanilladot", "polydot", "besseldot", "anovadot", "splinedot")) && "automatic" == kpar ) {
-					# cat(" Setting default kernel parameters ","\n");
-					kpar <- list();
-					}
-				}
-			}
-
-		if (!is.function(kernel)) {
-			if (!is.list(kpar) && is.character(kpar) && c(class(kernel) %in% c("rbfkernel", "laplacedot") || kernel %in% c("laplacedot", "rbfdot"))) {
-				kp <- match.arg(kpar, "automatic");
-				if("automatic" == kp) {
-					kpar <- list(sigma = mean(sigest(x, scaled = FALSE)[c(1,3)]));
-					}
-				# cat("Using automatic sigma estimation (sigest) for RBF or laplace kernel","\n");
-				}
-			}
-
-		if(!is(kernel,"kernel")) {
-			if (is(kernel,"function")) {kernel <- deparse(substitute(kernel));}
-			kernel <- do.call(kernel, kpar);
-			}
-		if(!is(kernel,"kernel")) {stop("kernel must inherit from class `kernel'");}
-
-		K <- kernelMatrix(kernel, x);
-		# cholesky decomposition K = R'R,  f = theta = K alpha = R' beta =>  R alpha = beta
-		R <- chol(K);
-		
-		lambda <- switch(family, 
-			'gaussian' = var/nrow(x),
-			1 / nrow(x)
-			);
-		beta <- as.vector(coef(
-				glmnet(t(R), y, alpha = 0, lambda = lambda, 
-					standardize = FALSE, family = family, ...)
-				));
-
-		if (!is.list(beta)) {
-			if (length(beta) > nrow(x)) {
-				alpha <- c(beta[1], backsolve(R, beta[-1]));
-			} else {
-				alpha <- c(0, backsolve(R, beta));
-				}
-		} else {
-			alpha <- lapply(beta, function(iBeta) {
-				iBeta <- as.vector(iBeta);
-				if (length(iBeta) > nrow(x)) {
-					c(iBeta[1], backsolve(R, iBeta[-1]));
-				} else {
-					c(0, backsolve(R, iBeta));
-					}
-				});
-			}
-		return(structure(
-			list(
-				alpha = alpha, 
-				kernel = kernel,
-				xmatrix = x,
-				x.scale = x.scale,
-				y.scale = y.scale,
-				family = family,
-				lev = if(is.factor(y)) levels(y) else NULL
-				), 
-			class = 'gpsrc'
-			));
 		}
+	if(is.character(kernel)) {
+		kernel <- match.arg(
+			kernel, 
+			c(
+				"rbfdot","polydot","tanhdot","vanilladot",
+				"laplacedot","besseldot","anovadot","splinedot"
+				)
+			);
+		if(is.character(kpar)) {
+			if((kernel %in% c("tanhdot", "vanilladot", "polydot", "besseldot", "anovadot", "splinedot")) && "automatic" == kpar ) {
+				# cat(" Setting default kernel parameters ","\n");
+				kpar <- list();
+				}
+			}
+		}
+
+	if (!is.function(kernel)) {
+		if (!is.list(kpar) && is.character(kpar) && c(class(kernel) %in% c("rbfkernel", "laplacedot") || kernel %in% c("laplacedot", "rbfdot"))) {
+			kp <- match.arg(kpar, "automatic");
+			if("automatic" == kp) {
+				kpar <- list(sigma = mean(sigest(x, scaled = FALSE)[c(1,3)]));
+				}
+			# cat("Using automatic sigma estimation (sigest) for RBF or laplace kernel","\n");
+			}
+		}
+
+	if(!is(kernel,"kernel")) {
+		if (is(kernel,"function")) {kernel <- deparse(substitute(kernel));}
+		kernel <- do.call(kernel, kpar);
+		}
+	if(!is(kernel,"kernel")) {stop("kernel must inherit from class `kernel'");}
+
+	K <- kernelMatrix(kernel, x);
+	# cholesky decomposition K = R'R,  f = theta = K alpha = R' beta =>  R alpha = beta
+	R <- chol(K);
+	
+	lambda <- switch(family, 
+		'gaussian' = var/nrow(x),
+		1 / nrow(x)
+		);
+	beta <- as.vector(coef(
+			glmnet(t(R), y, alpha = 0, lambda = lambda, 
+				standardize = FALSE, family = family, ...)
+			));
+
+	if (!is.list(beta)) {
+		if (length(beta) > nrow(x)) {
+			alpha <- c(beta[1], backsolve(R, beta[-1]));
+		} else {
+			alpha <- c(0, backsolve(R, beta));
+			}
+	} else {
+		alpha <- lapply(beta, function(iBeta) {
+			iBeta <- as.vector(iBeta);
+			if (length(iBeta) > nrow(x)) {
+				c(iBeta[1], backsolve(R, iBeta[-1]));
+			} else {
+				c(0, backsolve(R, iBeta));
+				}
+			});
+		}
+	return(structure(
+		list(
+			alpha = alpha, 
+			kernel = kernel,
+			xmatrix = x,
+			x.scale = x.scale,
+			y.scale = y.scale,
+			family = family,
+			lev = if(is.factor(y)) levels(y) else NULL
+			), 
+		class = 'gpsrc'
+		));
+	}
 
 #' @param object 'gpsrc' object from \code{gpsrc}
 #' @param newdata Design matrix of test set
